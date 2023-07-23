687e65530d8c29d2c6a03626e09f81312e1552cf
pabbarajupavan <pavankumar24@live.com>
2023-07-23 08:27:43 +0530

    Project Created

---
+++ smsrestservices/src/main/java/com/sms/SmsrestservicesApplication.java
@@ -0,0 +1,13 @@
+package com.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsrestservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsrestservicesApplication.class, args);
+	}
+
+}
