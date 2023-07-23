3bce42a3eaa1ce20d491ff02f8ddbde606baf9ea
pabbarajupavan <pavankumar24@live.com>
2023-07-23 20:36:15 +0530

    added a rest call for enrolling student

--- smsrestservices/src/main/java/com/sms/controller/AdminController.java
+++ smsrestservices/src/main/java/com/sms/controller/AdminController.java
@@ -1,21 +1,34 @@
 package com.sms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sms.beans.StudentMasterRequest;
+import com.sms.service.StudentMasterService;
 
 @RequestMapping("/api")
 public class AdminController {
 	
+	@Autowired
+	private StudentMasterService studentMasterService ;
+	
+	/**
+	 * 
+	 * Rest call to add students to the student master table
+	 * From request as json object**/
 	@RequestMapping(value = "/addstudentmaster", method = RequestMethod.POST)
 	private String addStudentsToMasterTable(@RequestBody StudentMasterRequest request) {
+		String response = null;
+		if (request != null && request.getReqHdr().getUserName() != null) {
+			response = studentMasterService.enrollStudent(request) ;
+		} else {
+			response = "Username is null" ;
+			
+		}
+		return response ;
 		
-		
-		
-		
-		return null;
 	}
 }
