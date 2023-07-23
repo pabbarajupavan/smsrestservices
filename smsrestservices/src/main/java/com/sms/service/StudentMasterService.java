c3f82c4a20842c8fadd2790f105e1fa4e520e3cd
pabbarajupavan <pavankumar24@live.com>
2023-07-23 20:08:12 +0530

    removed service annotation

--- smsrestservices/src/main/java/com/sms/service/StudentMasterService.java
+++ smsrestservices/src/main/java/com/sms/service/StudentMasterService.java
@@ -1,10 +1,8 @@
 package com.sms.service;
 
-import org.springframework.stereotype.Service;
-
 import com.sms.beans.StudentMasterRequest;
 
-@Service("studentMasterService")
+
 public interface StudentMasterService {
 	
 	public String enrollStudent(StudentMasterRequest request) ;
