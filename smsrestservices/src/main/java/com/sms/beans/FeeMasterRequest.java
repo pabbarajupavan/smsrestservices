c482f4ee858644c0c496f8feec5b37911f560f43
pabbarajupavan <pavankumar24@live.com>
2023-07-29 13:01:32 +0530

    FeeMaster request bean

---
+++ smsrestservices/src/main/java/com/sms/beans/FeeMasterRequest.java
@@ -0,0 +1,42 @@
+package com.sms.beans;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+public class FeeMasterRequest {
+	
+	private RequestHeader reqHdr ;
+	private String syllabusType ;
+	
+
+	private String sClass ;
+	
+
+	private int tutionFee ;
+	
+
+	private int transportFee ;
+	
+
+	private int accessoryFee ;
+	
+	
+	private int libraryFee ;
+	
+
+	private int examFee ;
+	
+	private int hostelFee ;
+	
+
+	private int annualFee ;
+	
+
+	private int discount ;
+	
+	
+
+}
