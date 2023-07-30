89f8f052eb80d7da3c1c6b3b0ea513ae7775da88
pabbarajupavan <pavankumar24@live.com>
2023-07-30 09:48:23 +0530

    added a method to fetch student info

--- smsrestservices/src/main/java/com/sms/service/StudentMasterService.java
+++ smsrestservices/src/main/java/com/sms/service/StudentMasterService.java
@@ -1,10 +1,13 @@
 package com.sms.service;
 
 import com.sms.beans.StudentMasterRequest;
+import com.sms.beans.StudentResponse;
 
 
 public interface StudentMasterService {
 	
 	public String enrollStudent(StudentMasterRequest request) ;
+
+	public StudentResponse fetchStudentInfoForFeePayaments(String admissionNumber);
 
 }
